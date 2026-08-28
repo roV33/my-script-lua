513ab3f49bdee846dfe585222047bdf6ed05c25b
R.OV333 <rofizaidan56@gmail.com>
2026-08-28 18:49:32 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,4 @@
-local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/master/source.lua", true))()
-
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/master/source.lua",))()
 
 local Window = Luna:CreateWindow({
 	Name = "Colz Hub", 
